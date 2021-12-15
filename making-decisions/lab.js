2334d1355dcdbba71bdb3cf0b05fb3d9b9b1760c
Cory Owen <cjowen14@outlook.com>
2021-12-15 15:07:43 -0700

    Finished Problem 9

--- making-decisions/lab.js
+++ making-decisions/lab.js
@@ -137,6 +137,15 @@ while(score !== passingScore){
 */
 
 //CODE HERE
+let changeMyMind = false;
+
+if(changeMyMind === true){
+  changeMyMind = false;
+  console.log(changeMyMind);
+} else{
+  changeMyMind = true;
+  console.log(changeMyMind);
+}
 
 ////////// PROBLEM 10 //////////
 // Try to change the value of changeMyMind (irrespective of whether it is true or false) and then console.log it's new status. It should log true. Hint: use the not operator.
