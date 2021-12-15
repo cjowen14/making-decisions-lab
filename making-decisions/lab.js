cffde2aa21f3fa361b0f4aba415efa471477aa1f
Cory Owen <cjowen14@outlook.com>
2021-12-15 14:33:20 -0700

    Finished Problem 2

--- making-decisions/lab.js
+++ making-decisions/lab.js
@@ -29,7 +29,13 @@ var brittanisBirthYear = 1986
 */
 
 //CODE HERE
-
+if (amysAge > brittanisAge){
+  console.log(`Amy is older`);
+} else if (brittanisAge > amysAge) {
+  console.log(`Brittani is older`);
+} else {
+  console.log(`They are the same age!`);
+}
 
 ////////// PROBLEM 3 //////////
 
